ce2675085aa393c916a193be7ec7a483c6419411
ze4dori <162151156+ze4dori@users.noreply.github.com>
2025-04-12 23:13:01 +0300

    Update config.py

--- config.py
+++ config.py
@@ -4,13 +4,13 @@ from dotenv import load_dotenv
 load_dotenv()  # Загружаем данные из .env файла
 
 class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY', 'a2f6d29e9b21c5f3b42a0539b4176f4f')
+    SECRET_KEY = os.getenv('SECRET_KEY')
     MAIL_SERVER = 'mail.hosting.reg.ru'
     MAIL_PORT = 465
     MAIL_USE_SSL = True
-    MAIL_USERNAME = 'info@digi-map.ru'
-    MAIL_PASSWORD = 'qQ3vU2rR3awT1mH3'
-    MAIL_DEFAULT_SENDER = 'info@digi-map.ru'
+    MAIL_USERNAME = ''
+    MAIL_PASSWORD = ''
+    MAIL_DEFAULT_SENDER = ''
     MAIL_RECIPIENT = os.getenv('MAIL_RECIPIENT')
 
     #БД
@@ -21,12 +21,12 @@ class Config:
     DB_NAME = os.getenv('DB_NAME')
 
     #ПАРСЕР
-    API_KEY = os.getenv('API_KEY', 'a9f900439663e569726c8e975c06ae80')
+    API_KEY = os.getenv('API_KEY')
     BASE_URL = os.getenv('BASE_URL', 'http://api.scraperapi.com')
 
     #ЕСИА
     GOSUSLUGI_AUTH_URL = os.getenv("GOSUSLUGI_AUTH_URL", "https://esia-portal1.test.gosuslugi.ru/aas/oauth2/ac")
     # GOSUSLUGI_AUTH_URL = "https://esia.gosuslugi.ru/aas/oauth2/ac"
-    CLIENT_ID = os.getenv("GOSUSLUGI_CLIENT_ID", "DIGIMAP")
-    REDIRECT_URI = os.getenv("GOSUSLUGI_REDIRECT_URI", "https://digi-map.ru/")
+    CLIENT_ID = os.getenv("GOSUSLUGI_CLIENT_ID")
+    REDIRECT_URI = os.getenv("GOSUSLUGI_REDIRECT_URI")
     THUMBPRINT = os.getenv("PRIVATE_KEY_THUMBPRINT")
